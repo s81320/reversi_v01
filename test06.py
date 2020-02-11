879840474461ec5e10d946d0a0269cb5868dbdcf
Silke Meiner <s81320@beuh-hochschule.de>
2020-02-11 09:42:07 +0100

    second test function

--- test06.py
+++ test06.py
@@ -20,6 +20,21 @@ class TestObjectCreation(TestCase):
 		self.assertIsInstance(b.board,dict)
 		self.assertEqual(len(b.board), b.max_number_stones)
 
+	def test_check_position_exists(self):
+		mock_host = Mock(spec=reversi.Host)
+		b = reversi.Board(mock_host)
+		self.assertEqual(b.check_position_exists((9,9)), False)
+		self.assertEqual(b.check_position_exists((3,19)), False)
+		self.assertEqual(b.check_position_exists((13,4)), False)
+		self.assertEqual(b.check_position_exists((9,'a')), False)
+		self.assertEqual(b.check_position_exists(('+','a')), False)
+
+		self.assertEqual(b.check_position_exists((1,2)), True)
+		self.assertEqual(b.check_position_exists((7,5)), True)
+		self.assertEqual(b.check_position_exists((0,0)), True)
+		self.assertEqual(b.check_position_exists((2,4)), True)
+		self.assertEqual(b.check_position_exists((5,7)), True)
+
 	def test_creation_host(self):
 		pass
 
